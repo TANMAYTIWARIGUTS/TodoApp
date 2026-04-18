8e5d6c07a9e8b48ee69dad1420c43deefe371f37
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-04-18 21:08:19 +0530

    implementing behaviour

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("I am On");
+const todoForm=document.querySelector("form");
+const todoInput=document.getElementById("todo-input");
+const todoListUL=document.getElementById("todo-list")
+
+const allTodos=[];
+
+todoForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    addTodo();
+});
+
+function addTodo(){
+    const todoText=todoInput.value.trim();
+    if(todoText.length>0){
+        allTodos.push(todoText);
+        createTodoItem(todoText);
+        todoInput.value="";
+    }
+}
+
+function updateTodoList(){
+    todoListUL.innerHTML="";
+    allTodos.forEach((todo, todoIndex)=>{
+        todoItem=createTodoItem(todo, todoIndex)
+    })
+}
+
+function createTodoItem(todo) {
+    const todoLI=document.createElement("li");
+    todoLI.innerHTML=todo;
+    return todoLI;
+    
+} 
