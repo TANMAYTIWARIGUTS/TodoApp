9d7706d1bd2ffb509ac42df26bacdf4479472b6e
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-04-19 23:37:40 +0530

    js implemented && localStorage to go

--- script.js
+++ script.js
@@ -1,42 +1,50 @@
-const form=document.querySelector("form");
-const inpVal=document.getElementById("todo-input");
-const ul=document.querySelector("ul");
+const todoForm=document.querySelector("form");
+const todoInput=document.getElementById("todo-input");
+const todoListUL=document.getElementById("todo-list");
 
-form.addEventListener("submit",(e)=>{
+const allTodos=[];
+
+
+todoForm.addEventListener("submit",(e)=>{
     e.preventDefault();
-    // console.log(inpVal.value.trim());
-    addTodo();
-    
+    addTodos();
+   
 })
 
-function addTodo(){
-    const todoValue= inpVal.value.trim();
-    if(todoValue.length>0){
-        inpVal.value="";
-        // console.log(todoValue);
-        display(todoValue);
+function addTodos(){
+    const todoText=todoInput.value.trim();
+    console.log(todoText);
+    if(todoText.length > 0){
+        allTodos.push(todoText);
+        todoInput.value="";
+        updateTodoList();
     }
 }
 
-function display(todoValue){
+function updateTodoList(){
+    todoListUL.innerHTML="";
+    allTodos.forEach((todo, todoIndex)=>{
+        createTodoItem(todo, todoIndex);
+    })
+}
+function createTodoItem(todo, todoIndex){
     const li=document.createElement("li");
-    li.classList.add("todo")
+    li.classList.add("todo");
     li.innerHTML=`
-                <input type="checkbox" name="" id="todo-1">
-                <label class="custom-checkbox" for="todo-1">
+                <input type="checkbox" name="" id="todo-${todoIndex}">
+                <label class="custom-checkbox" for="todo-${todoIndex}">
                     <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
                         fill="#000000">
                         <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
                     </svg>
                 </label>
-                <label for="todo-1" class="todo-text">
-                    ${todoValue}
+                <label for="todo-${todoIndex}" class="todo-text">
+                    ${todo}
                 </label>
                 <button class="delete-button">
                     <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
                 </button>
-    `;
-    
-    ul.prepend(li);
-    
+    `
+    todoListUL.append(li)
 }
+
